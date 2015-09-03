f26bf965927abec608a9970c876e4e277983e54d
SchizoAA <kyle.erickson15@gmail.com>
2015-09-02 21:16:40 -0600

    Completed Assignment.

--- Assignment1/Assignment1/kilos_to_lbs.cpp
+++ Assignment1/Assignment1/kilos_to_lbs.cpp
@@ -2,7 +2,9 @@
 -accept a single command line numerical argument. You may assume a valid number is the argument.
 -convert that number to lbs
 -output only the converted number to cout and finish with an endl
-  -The output should round to two decimal places of precision after the decimal point. (hint, research the fixed command).
+-The output should round to two decimal places of precision after the decimal point. (hint, research the fixed command).
+
+Bash command for line-at-a-time execution: cat values.txt | while read line; do ./kilos_to_lbs.out $line; done
 
 NAME: Kyle Erickson
 Uid: U0461601
@@ -10,16 +12,22 @@ DATE: 08/27/2015
 */
 
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
-int main(char* in)
+int main(int argc, char** argv)
 {
-	cout.precision(3);
-	in = "1.6";//REMOVE: test line for input
-	double kilos = atof(in);
+	if (argc <= 1)//check for command line input
+	{
+		cout << "No argument entered. Please provide an input." << endl;
+	}
+
+	double kilos = atof(argv[argc - 1]);//Take in argument from command line
 	double lbs = kilos * 2.20462;//Convert kilos to lbs
-	cout << lbs << fixed << endl;
-	
+
+	cout.precision(2);
+	cout << fixed << lbs << endl;
 	return 0;
 }
